8f0cb4093b1c69c8c94e86e685be6943c1c93f11
Sean Shubin <sshubin@cj.com>
2020-05-02 13:16:23 -0700

    ratio

---
+++ domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/ratio/Ratio.kt
@@ -0,0 +1,51 @@
+package com.seanshubin.kotlin.tryme.domain.ratio
+
+data class Ratio(val numerator: Int, val denominator: Int) : Comparable<Ratio> {
+  operator fun plus(that: Ratio): Ratio {
+    val lcm = leastCommonMultiple(denominator, that.denominator)
+    return Ratio(numerator * lcm / denominator + that.numerator * lcm / that.denominator, lcm).simplify()
+  }
+
+  operator fun minus(that: Ratio): Ratio = (this + -that).simplify()
+  operator fun times(that: Ratio): Ratio = Ratio(numerator * that.numerator, denominator * that.denominator).simplify()
+  operator fun div(that: Ratio): Ratio = (this * that.recriprocal()).simplify()
+  operator fun unaryMinus(): Ratio = Ratio(-numerator, denominator).simplify()
+  fun recriprocal(): Ratio = Ratio(denominator, numerator).simplify()
+  fun withDenominator(newDenominator: Int): Ratio = Ratio(numerator * newDenominator / denominator, newDenominator)
+  override fun compareTo(that: Ratio): Int {
+    val lcm = leastCommonMultiple(denominator, that.denominator)
+    return (numerator * lcm / denominator).compareTo(that.numerator * lcm / that.denominator)
+  }
+
+  override fun toString(): String = "$numerator/$denominator"
+
+  fun simplify(): Ratio = simplifyFactor().simplifySign()
+  private fun simplifySign(): Ratio =
+      if (denominator < 0) Ratio(-numerator, -denominator)
+      else this
+
+  private fun simplifyFactor(): Ratio {
+    val gcf = greatestCommonFactor(numerator, denominator)
+    return Ratio(numerator / gcf, denominator / gcf)
+  }
+
+  companion object {
+    fun greatestCommonFactor(a: Int, b: Int): Int =
+        if (b == 0) a
+        else greatestCommonFactor(b, a % b)
+
+    fun leastCommonMultiple(a: Int, b: Int): Int =
+        if (a == 0 && b == 0) 0
+        else a * b / greatestCommonFactor(a, b)
+
+    val regex = Regex("""(-?\d+)/(-?\d+)""")
+
+    fun parse(s: String): Ratio {
+      val matchResult = regex.matchEntire(s)
+      if (matchResult == null) throw RuntimeException("Value '$s' could did not match expression $regex")
+      val numerator = matchResult.groupValues[1].toInt()
+      val denominator = matchResult.groupValues[2].toInt()
+      return Ratio(numerator, denominator).simplify()
+    }
+  }
+}
---
+++ domain/src/test/kotlin/com/seanshubin/kotlin/tryme/domain/ratio/RatioTest.kt
@@ -0,0 +1,41 @@
+package com.seanshubin.kotlin.tryme.domain.ratio
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class RatioTest {
+  @Test
+  fun add() {
+    assertAdd("3/5", "1/2", "11/10")
+    assertAdd("3/-5", "-1/-2", "-1/10")
+  }
+
+  @Test
+  fun negate() {
+    assertNegate("3/5", "-3/5")
+    assertNegate("1/-5", "1/5")
+  }
+
+  @Test
+  fun subtract() {
+    assertSubtract("3/5", "1/2", "1/10")
+    assertSubtract("1/2", "3/5", "-1/10")
+    assertSubtract("-3/5", "1/2", "-11/10")
+    assertSubtract("1/-2", "-3/-5", "-11/10")
+  }
+
+  fun assertAdd(a: String, b: String, expected: String) {
+    val actual = (Ratio.parse(a) + Ratio.parse(b)).toString()
+    assertEquals(expected, actual, "$a + $b")
+  }
+
+  fun assertSubtract(a: String, b: String, expected: String) {
+    val actual = (Ratio.parse(a) - Ratio.parse(b)).toString()
+    assertEquals(expected, actual, "$a + $b")
+  }
+
+  fun assertNegate(a: String, expected: String) {
+    val actual = (-Ratio.parse(a)).toString()
+    assertEquals(expected, actual, "-$a")
+  }
+}
