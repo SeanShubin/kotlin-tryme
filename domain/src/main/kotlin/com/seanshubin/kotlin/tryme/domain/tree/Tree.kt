209b607b3684be349b2855b81505fead36238d0a
SeanShubin <seanshubin@gmail.com>
2020-05-03 22:47:50 -0700

    tree tests

--- domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/tree/Tree.kt
+++ domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/tree/Tree.kt
@@ -1,14 +1,44 @@
 package com.seanshubin.kotlin.tryme.domain.tree
 
 data class Tree<T>(val value: T,
-                   val children: List<Tree<T>>) {
+                   val children: List<Tree<T>> = emptyList()) {
   fun <U> map(f: (T) -> U): Tree<U> {
     val newValue = f(value)
     val newChildren = children.map { it.map(f) }
     return Tree(newValue, newChildren)
   }
 
+  fun addChildPath(vararg parts: T): Tree<T> = addChildPath(parts.toList())
+
+  fun addChildPath(parts: List<T>): Tree<T> {
+    if (parts.isEmpty()) return this
+    val part = parts[0]
+    val existingIndex = children.indexOfFirst { it.value == part }
+    return if (existingIndex == -1) {
+      val remainingPath = parts.drop(1)
+      val newChild = Tree(part, emptyList()).addChildPath(remainingPath)
+      val newChildren = children + newChild
+      Tree(value, newChildren)
+    } else {
+      val remainingPath = parts.drop(1)
+      val existingChild = children[existingIndex]
+      val newChild = existingChild.addChildPath(remainingPath)
+      val newChildren = children.take(existingIndex) + newChild + children.drop(existingIndex + 1)
+      Tree(value, newChildren)
+    }
+  }
+
   fun <U> mapWithCoordinates(f: (T, List<T>, List<Int>) -> U): Tree<U> = mapWithCoordinates(emptyList(), emptyList(), f)
+
+  fun forEach(f: (Tree<T>) -> Unit) {
+    f(this)
+    children.forEach { it.forEach(f) }
+  }
+
+  fun forEachWithCoordinates(f: (Tree<T>, List<T>, List<Int>) -> Unit) =
+          forEachWithCoordinates(emptyList(), emptyList(), f)
+
+  fun toList(): List<T> = listOf(value) + children.flatMap { it.toList() }
 
   private fun <U> mapWithCoordinates(ancestors: List<T>, indices: List<Int>, f: (T, List<T>, List<Int>) -> U): Tree<U> {
     val newValue = f(value, ancestors, indices)
@@ -19,14 +49,6 @@ data class Tree<T>(val value: T,
     }
     return Tree(newValue, newChildren)
   }
-
-  fun forEach(f: (Tree<T>) -> Unit) {
-    f(this)
-    children.forEach { it.forEach(f) }
-  }
-
-  fun forEachWithCoordinates(f: (Tree<T>, List<T>, List<Int>) -> Unit) =
-      forEachWithCoordinates(emptyList(), emptyList(), f)
 
   private fun <U> forEachWithCoordinates(ancestors: List<T>, indices: List<Int>, f: (Tree<T>, List<T>, List<Int>) -> U) {
     f(this, ancestors, indices)
---
+++ domain/src/test/kotlin/com/seanshubin/kotlin/tryme/domain/tree/TreeTest.kt
@@ -0,0 +1,77 @@
+package com.seanshubin.kotlin.tryme.domain.tree
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class TreeTest {
+    @Test
+    fun createWithConstructor() {
+        val b = Tree("b.txt")
+        val c = Tree("c.txt")
+        val g = Tree("g.txt")
+        val a = Tree("a", listOf(b, c))
+        val e = Tree("e")
+        val f = Tree("f", listOf(g))
+        val treeSample = Tree("tree-sample", listOf(a, e, f))
+        val indentedDisplay: (String, List<String>, List<Int>) -> String = { value, _, indices ->
+            "  ".repeat(indices.size) + value
+        }
+        val actual = treeSample.mapWithCoordinates(indentedDisplay).toList()
+        val expected = listOf(
+                "tree-sample",
+                "  a",
+                "    b.txt",
+                "    c.txt",
+                "  e",
+                "  f",
+                "    g.txt")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun createWithHelpers() {
+        val treeSample = Tree("tree-sample")
+                .addChildPath("a", "b.txt")
+                .addChildPath("a", "c.txt")
+                .addChildPath("e")
+                .addChildPath("f", "g.txt")
+        val indentedDisplay: (String, List<String>, List<Int>) -> String = { value, _, indices ->
+            "  ".repeat(indices.size) + value
+        }
+        val actual = treeSample.mapWithCoordinates(indentedDisplay).toList()
+        val expected = listOf(
+                "tree-sample",
+                "  a",
+                "    b.txt",
+                "    c.txt",
+                "  e",
+                "  f",
+                "    g.txt")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun map() {
+        val treeSample = Tree(2)
+                .addChildPath(3, 4, 5)
+                .addChildPath(3, 4, 6)
+                .addChildPath(4, 6, 7)
+                .addChildPath(4, 6, 8)
+                .addChildPath(4, 6, 9)
+                .addChildPath(5, 5, 7)
+                .addChildPath(5, 6, 8)
+                .addChildPath(5, 7, 9)
+        val expected = Tree(4)
+                .addChildPath(9, 16, 25)
+                .addChildPath(9, 16, 36)
+                .addChildPath(16, 36, 49)
+                .addChildPath(16, 36, 64)
+                .addChildPath(16, 36, 81)
+                .addChildPath(25, 25, 49)
+                .addChildPath(25, 36, 64)
+                .addChildPath(25, 49, 81)
+        val squared = { x: Int -> x * x }
+        val actual = treeSample.map(squared)
+        assertEquals(expected, actual)
+    }
+}
