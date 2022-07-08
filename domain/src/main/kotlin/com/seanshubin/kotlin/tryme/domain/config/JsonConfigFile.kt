271570150120c21a7dcd2b00f1b0041cfaf1add7
Sean Shubin <sshubin@cj.com>
2022-07-07 20:24:52 -0700

    config

---
+++ domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/config/JsonConfigFile.kt
@@ -0,0 +1,46 @@
+package com.seanshubin.kotlin.tryme.domain.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.seanshubin.kotlin.tryme.domain.contract.FilesContract
+import java.nio.file.Path
+
+class JsonConfigFile(
+    val files: FilesContract,
+    val configFilePath: Path
+) {
+    fun loadInt(default: Any?, vararg pathParts: String): () -> Int = {
+        loadUntyped(default, *pathParts).asInt()
+    }
+
+    fun loadString(default: Any?, vararg pathParts: String): () -> String = {
+        loadUntyped(default, *pathParts).asString()
+    }
+
+    private fun loadUntyped(default: Any?, vararg pathParts: String): Untyped {
+        return if (files.exists(configFilePath)) {
+            val text = files.readString(configFilePath)
+            val untyped = Untyped(parser.readValue<Any?>(text))
+            if (untyped.valueExistsAtPath(*pathParts)) {
+                untyped.getValueAtPath(*pathParts)
+            } else {
+                val newUntyped = untyped.setValueAtPath(default, *pathParts)
+                val jsonText = pretty.writeValueAsString(newUntyped.value)
+                files.writeString(configFilePath, jsonText)
+                loadUntyped(default, *pathParts)
+            }
+        } else {
+            files.writeString(configFilePath, "{}")
+            loadUntyped(default, *pathParts)
+        }
+    }
+
+
+    companion object {
+        val pretty: ObjectMapper = ObjectMapper().registerKotlinModule().enable(SerializationFeature.INDENT_OUTPUT)
+        val compact: ObjectMapper = ObjectMapper().registerKotlinModule()
+        val parser: ObjectMapper = compact
+    }
+}
---
+++ domain/src/test/kotlin/com/seanshubin/kotlin/tryme/domain/config/JsonConfigFileTest.kt
@@ -0,0 +1,70 @@
+package com.seanshubin.kotlin.tryme.domain.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.seanshubin.kotlin.tryme.domain.contract.FilesContract
+import com.seanshubin.kotlin.tryme.domain.contract.FilesContractNotImplemented
+import com.seanshubin.kotlin.tryme.domain.contract.FilesDelegate
+import org.junit.Test
+import java.nio.file.LinkOption
+import java.nio.file.OpenOption
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.test.assertEquals
+
+class JsonConfigFileTest {
+    @Test
+    fun loadDefaultInt() {
+        // given
+        val files: FilesContract = FakeFiles()
+        val configFilePath = Paths.get("test-config.json")
+        val jsonConfigFile = JsonConfigFile(files, configFilePath)
+        val loadB = jsonConfigFile.loadInt(123, "a", "b")
+        val expectedValue = 123
+        val expectedJson =
+            """{
+              |  "a": {
+              |    "b": 123
+              |  }
+              |}""".trimMargin()
+
+        // when
+        val actualValue = loadB()
+
+        // then
+        val actualJson = files.readString(configFilePath)
+        assertEquals(expectedValue, actualValue)
+        assertJsonEquals(expectedJson, actualJson)
+    }
+
+    class FakeFiles:FilesContractNotImplemented{
+        val contentMap:MutableMap<String, String> = mutableMapOf()
+        override fun exists(path: Path, vararg options: LinkOption): Boolean {
+            return contentMap.containsKey(path.toString())
+        }
+
+        override fun writeString(path: Path, csq: CharSequence, vararg options: OpenOption): Path {
+            contentMap[path.toString()] = csq.toString()
+            return path
+        }
+
+        override fun readString(path: Path): String {
+            return contentMap.getValue(path.toString())
+        }
+    }
+
+    fun assertJsonEquals(expected:String, actual:String){
+        assertEquals(expected.prettyJson(), actual.prettyJson())
+    }
+
+    val pretty: ObjectMapper = ObjectMapper().registerKotlinModule().enable(SerializationFeature.INDENT_OUTPUT)
+
+    fun String.prettyJson():String {
+        val asObject = pretty.readValue<Any>(this)
+        val asNormalized = pretty.writeValueAsString(asObject)
+        return asNormalized
+
+    }
+}
