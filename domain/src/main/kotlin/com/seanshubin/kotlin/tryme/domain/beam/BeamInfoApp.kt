abc67c5f43c01bdf55c6d4adcf05b77610c0473a
Sean Shubin <sshubin@cj.com>
2023-03-26 13:54:20 -0700

    display atoms, imports, exports

--- domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/beam/BeamInfoApp.kt
+++ domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/beam/BeamInfoApp.kt
@@ -26,19 +26,40 @@ object BeamInfoApp {
         val assembler = AssemblerImpl()
         val assembled = assembler.assemble(target, tree) as BeamFile
         val sectionsByName = assembled.sections.associate {
-            it.name to it.bytes
+            it.name to it
         }
-        val atomBytes = sectionsByName.getValue("AtU8")
-        val importTableBytes = sectionsByName.getValue("ImpT")
-        val atoms = extractAtoms(atomBytes)
-        val importEntries = extractImportEntries(importTableBytes)
-        println(atoms)
-        println(atoms.size)
-        println(importEntries)
-        println(importEntries.size)
+        val atomSection = sectionsByName.getValue("AtU8")
+        val importTableSection = sectionsByName.getValue("ImpT")
+        val exportTableSection = sectionsByName.getValue("ExpT")
+        val atoms = extractAtoms(atomSection.bytes)
+        val importEntries = extractImportEntries(importTableSection.bytes)
         val imports = composeImports(atoms, importEntries)
+        val exportEntries= extractExportEntries(exportTableSection.bytes)
+        val exports = composeExports(atoms, exportEntries)
+        println("atoms")
+        atoms.forEachIndexed { index, atom ->
+            println("${index+1} $atom")
+        }
+        println()
+
+        println("import entries")
+        importEntries.forEach(::println)
+        println(importEntries.size)
+        println()
+
+        println("imports")
         imports.forEach(::println)
         println(imports.size)
+        println()
+
+        println("export entries")
+        exportEntries.forEach(::println)
+        println(exportEntries.size)
+        println()
+
+        println("exports")
+        exports.forEach(::println)
+        println(exports.size)
     }
 
     fun displaySection(section:Section){
@@ -79,6 +100,23 @@ object BeamInfoApp {
         return importEntries
     }
 
+    fun extractExportEntries(bytes:List<Byte>):List<ExportEntry>{
+        var remaining = bytes
+        val count = bytes.take(4).toInt()
+        remaining = remaining.drop(4)
+        val exportEntries = mutableListOf<ExportEntry>()
+        (0 until count).map {
+            val function = remaining.take(4).toInt()
+            remaining = remaining.drop(4)
+            val arity = remaining.take(4).toInt()
+            remaining = remaining.drop(4)
+            val label = remaining.take(4).toInt()
+            remaining = remaining.drop(4)
+            exportEntries.add(ExportEntry(function, arity, label))
+        }
+        return exportEntries
+    }
+
     fun composeImports(atoms:List<String>,importEntries:List<ImportEntry> ):List<Import>{
         val imports = importEntries.map { importEntry ->
             val module = atoms[importEntry.module-1]
@@ -88,4 +126,23 @@ object BeamInfoApp {
         }
         return imports
     }
+
+    fun composeExports(atoms:List<String>,exportEntries:List<ExportEntry> ):List<Export>{
+        val exports = exportEntries.map { exportEntry ->
+            val function = atoms[exportEntry.function-1]
+            val arity = exportEntry.arity
+            val label = atoms[exportEntry.label-1]
+            Export(function, arity, label)
+        }
+        return exports
+    }
 }
+
+/*
+00 00 00 04
+00 00 00 20 00 00 00 01 00 00 00 11
+00 00 00 20 00 00 00 00 00 00 00 0F
+00 00 00 0F 00 00 00 00 00 00 00 0B
+00 00 00 02 00 00 00 01 00 00 00 02                                     ....
+
+ */
---
+++ domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/beam/Export.kt
@@ -0,0 +1,3 @@
+package com.seanshubin.kotlin.tryme.domain.beam
+
+data class Export(val function:String, val arity:Int, val label:String)
