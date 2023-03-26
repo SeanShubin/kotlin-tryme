9479f2b39b6e26b808c8145df0449320b96d94b2
Sean Shubin <sshubin@cj.com>
2023-03-25 21:04:49 -0700

    Fixed align issue

--- domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/beam/ByteSequenceFixedSizeExpression.kt
+++ domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/beam/ByteSequenceFixedSizeExpression.kt
@@ -4,6 +4,7 @@ import com.seanshubin.kotlin.tryme.domain.cursor.Cursor
 
 class ByteSequenceFixedSizeExpression(override val name:String, private val size:Int):Expression {
     override fun parse(start: Cursor<Byte>): Result {
+        var discardForAlign = 3 - ((size+3) % 4)
         var current = start
         val list = mutableListOf<Tree<Byte>>()
         while(list.size < size){
@@ -11,6 +12,14 @@ class ByteSequenceFixedSizeExpression(override val name:String, private val size
             val actual = current.value
             list.add(Tree.Leaf("$name-byte", actual))
             current = current.next()
+        }
+        while(discardForAlign > 0){
+            if(current.isEnd){
+                discardForAlign = 0
+            } else {
+                current = current.next()
+                discardForAlign--
+            }
         }
         return Result.Success(current, Tree.Branch(name, list))
     }
