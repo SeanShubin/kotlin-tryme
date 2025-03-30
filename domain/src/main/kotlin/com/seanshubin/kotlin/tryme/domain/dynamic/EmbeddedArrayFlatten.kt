6675e2fa1b9704ead6e63acd76b1a6202de09ee0
Sean Shubin <sshubin@cj.com>
2025-03-30 13:49:32 -0700

    simplified

--- domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/dynamic/EmbeddedArrayFlatten.kt
+++ domain/src/main/kotlin/com/seanshubin/kotlin/tryme/domain/dynamic/EmbeddedArrayFlatten.kt
@@ -4,23 +4,22 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.seanshubin.kotlin.tryme.domain.json.JsonMappers
 
 object EmbeddedArrayFlatten {
-    fun flattenJson(jsonString:String, path:List<String>, referenceKey:String, referenceValue:Any?, indexKey:String):List<Any?>{
+    fun flattenJson(jsonString:String, path:List<String>, indexKey:String):List<Any?>{
         val o = JsonMappers.parser.readValue<Any?>(jsonString)
-        return flatten(o, path, referenceKey, referenceValue, indexKey)
+        return flatten(o, path, indexKey)
     }
 
-    fun flatten(o:Any?, path:List<Any?>, referenceKey:Any?, referenceValue:Any?, indexKey:Any?):List<Any?>{
-        return flattenAtPath(o, path, referenceKey, referenceValue, indexKey)
+    fun flatten(o:Any?, path:List<Any?>, indexKey:Any?):List<Any?>{
+        return flattenAtPath(o, path, indexKey)
     }
 
-    fun flattenAtPath(o:Any?, path:List<Any?>, referenceKey:Any?, referenceValue:Any?, indexKey:Any?):List<Any?> {
+    fun flattenAtPath(o:Any?, path:List<Any?>, indexKey:Any?):List<Any?> {
         val valueAtPath = ObjectKeyValueStore.getValue(o, path)
         if(valueAtPath is List<*>){
             return valueAtPath.mapIndexed { index, element ->
                 val a = ObjectKeyValueStore.setValue(o, path, element)
-                val b = ObjectKeyValueStore.setValue(a, path + referenceKey, referenceValue)
-                val c = ObjectKeyValueStore.setValue(b, path + indexKey, index)
-                c
+                val b = ObjectKeyValueStore.setValue(a, path + indexKey, index)
+                b
             }
         } else{
             throw  RuntimeException("List expected at path ${path.joinToString(",")}")
--- domain/src/test/kotlin/com/seanshubin/kotlin/tryme/domain/dynamic/EmbeddedArrayFlattenTest.kt
+++ domain/src/test/kotlin/com/seanshubin/kotlin/tryme/domain/dynamic/EmbeddedArrayFlattenTest.kt
@@ -28,7 +28,6 @@ class EmbeddedArrayFlattenTest {
                 "c": {
                   "d": 2,
                   "e": 3,
-                  "c-reference": 123,
                   "c-index": 0
                 }
               }
@@ -41,7 +40,6 @@ class EmbeddedArrayFlattenTest {
                 "c": {
                   "d": 4,
                   "e": 5,
-                  "c-reference": 123,
                   "c-index": 1
                 }
               }
@@ -54,18 +52,15 @@ class EmbeddedArrayFlattenTest {
                 "c": {
                   "d": 6,
                   "e": 7,
-                  "c-reference": 123,
                   "c-index": 2
                 }
               }
             }
             """.trimIndent()
         ).map{it.normalizeJson()}
-        val referenceKey = "c-reference"
-        val referenceValue = 123
         val indexKey = "c-index"
         val path = listOf("b", "c")
-        val actual = EmbeddedArrayFlatten.flattenJson(input, path, referenceKey, referenceValue, indexKey).map{it.toJson()}.map{it.normalizeJson()}
+        val actual = EmbeddedArrayFlatten.flattenJson(input, path, indexKey).map{it.toJson()}.map{it.normalizeJson()}
         assertEquals(expected, actual)
     }
 }
